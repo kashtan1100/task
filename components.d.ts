3d0e821e4f629fa28d00ed4885b2359b4304f352
kashtan1100 <61379997+kashtan1100@users.noreply.github.com>
2025-01-23 16:37:43 +0300

    deploy

--- components.d.ts
+++ components.d.ts
@@ -14,16 +14,7 @@ declare module 'vue' {
     BModal: typeof import('bootstrap-vue-next/components/BModal')['BModal']
     BPagination: typeof import('bootstrap-vue-next/components/BPagination')['BPagination']
     Filter: typeof import('./src/components/Filter.vue')['default']
-    FilterComponent: typeof import('./src/components/FilterComponent.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
